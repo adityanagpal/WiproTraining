207e53754e3a2e67e29f5db11c5399b18d1eb5bf
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-01 23:28:10 +0530

    Create Interest.java

---
+++ JavaFundamentals/FlowControlStatements/Interest.java
@@ -0,0 +1,43 @@
+/*
+Write a program to accept gender ("Male" or "Female") and age (1-120) from command line arguments and print
+the percentage of interest based on the given conditions.
+Interest == 8.2% 
+ Gender ==> Female
+ Age    ==>1 to 58
+Interest == 7.6% 
+ Gender ==> Female
+ Age    ==>59 -120
+Interest == 9.2% 
+ Gender ==> Male
+ Age    ==>1-60
+Interest == 8.3% 
+ Gender ==> Male
+ Age    ==>61-120
+ */
+ class Interest
+ {
+ public static void main(String arg[])
+ {
+ double interest=0.0;
+ int age = Integer.parseInt(arg[1]);
+ if(arg[0]=="Female")
+{
+  System.out.println("the interes");
+  if(age>0&&age<59)
+  interest=8.2;
+  else if(age>58&&age<121)
+  interest=7.6;
+}
+else if(arg[1]=="Male")
+{
+  if(age>0&&age<61)
+  interest=9.2;
+  else if(age>60&&age<121)
+  interest=8.3;
+}
+ else{
+ System.out.println("Invalid commandline arguments type Gender(full and first letter capital) and age(0-120)");
+}
+ System.out.println("the interest is :"+interest);
+ }
+ }
