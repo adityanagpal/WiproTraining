fac9042ece21d091a0de0b3f6d759c1deb756136
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 21:49:46 +0530

    Create Calculator.java

---
+++ OOPS/Classes/Calculator.java
@@ -0,0 +1,10 @@
+/*
+ Create a new class called “Calculator” which contains the following: 
+1. A static method called powerInt(int num1,int num2) that accepts two integers and returns num1 
+to the power of num2 (num1 power num2). 
+2. A static method called powerDouble(double num1,int num2) that accepts one double and one integer 
+and returns num1 to the power of num2 (num1 power num2). 
+3. Call your method from another class without instantiating the class (i.e. call it like Calculator.
+powerInt(12,10) since your methods are defined to be static)
+Hint: Use Math.pow(double,double) to calculate the power.
+*/
