3f78d0b2930a001938924f15ccc167adccaad4a5
AMAN VERMA <31860233+jnvamanverma@users.noreply.github.com>
2018-09-03 00:12:45 +0530

    Update Dublicate.java

--- JavaFundamentals/Arrays/Dublicate.java
+++ JavaFundamentals/Arrays/Dublicate.java
@@ -3,3 +3,32 @@ Write a program to remove the duplicate elements in an array and print
 Eg) Array Elements--12,34,12,45,67,89
 O/P: 12,34,45,67,89
 */
+public class Duplicate{  
+public static int removeDuplicateElements(int arr[], int n){  
+        if (n==0 || n==1){  
+            return n;  
+        }  
+        int[] temp = new int[n];  
+        int j = 0;  
+        for (int i=0; i<n-1; i++){  
+            if (arr[i] != arr[i+1]){  
+                temp[j++] = arr[i];  
+            }  
+         }  
+        temp[j++] = arr[n-1];     
+        // Changing original array  
+        for (int i=0; i<j; i++){  
+            arr[i] = temp[i];  
+        }  
+        return j;  
+    }  
+       
+    public static void main (String[] args) {  
+        int arr[] = {10,20,20,30,30,40,50,50};  
+        int length = arr.length;  
+        length = removeDuplicateElements(arr, length);  
+        //printing array elements  
+        for (int i=0; i<length; i++)  
+           System.out.print(arr[i]+" ");  
+    }  
+}  
