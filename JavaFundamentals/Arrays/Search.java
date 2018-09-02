dd18955623082909c23d878ee0f7f85dbc66931c
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 21:25:59 +0530

    Create Search.java

---
+++ JavaFundamentals/Arrays/Search.java
@@ -0,0 +1,9 @@
+/*
+Write a program to initialize an integer array with values and check if a given number 
+is present in the array or not. If the number is not found, it will print -1 else it 
+will print the index value of the given  number in the array
+Ex1) Array elements are  {1,4,34,56,7} and the search element is 90
+O/P: -1
+Ex2)Array elements are  {1,4,34,56,7} and the search element is 56
+O/P: 4 
+*/
