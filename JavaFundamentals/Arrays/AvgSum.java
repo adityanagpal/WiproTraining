55c57174c01d9ca8346a9e85c4ec69e3dc3e8a2a
AMAN VERMA <31860233+jnvamanverma@users.noreply.github.com>
2018-09-03 00:12:02 +0530

    Update AvgSum.java

--- JavaFundamentals/Arrays/AvgSum.java
+++ JavaFundamentals/Arrays/AvgSum.java
@@ -1,3 +1,14 @@
 /*
 Write a program to initialize an integer array and print the sum and average of the array.
 */
+public class AvgSum {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < args.length; i++) {
+            sum = sum + Integer.parseInt(args[i]);
+        }
+        System.out.println("The sum of the arguments passed is " + sum);
+	System.out.println("The average of the arguments passed is " + sum/args.length);
+    }
+}
