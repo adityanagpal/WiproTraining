866831c430756bdd858f728671f8a189bd1115c6
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 17:14:35 +0530

    Create CheckPrime.java

---
+++ JavaFundamentals/FlowControlStatements/CheckPrime.java
@@ -0,0 +1,33 @@
+/*
+Write a Java program to find if the given number is prime or not.
+
+Example1:
+
+     C:\>java Sample 
+
+     O/P Expected : Please enter an integer number 
+
+Example2:
+
+     C:\>java Sample 1
+
+     O/P Expected : 1 is neither prime nor composite
+
+Example3:
+
+     C:\>java Sample 0
+
+     O/P Expected : 0 is neither prime nor composite
+ 
+ Example4:
+
+     C:\>java Sample 10
+
+     O/P Expected : 10 is not a prime number
+
+Example5:
+
+     C:\>java Sample 7
+
+     O/P Expected : 7 is a prime number
+*/
