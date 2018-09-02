a4222b02bb4c1801dab8dd31badc606acd83c60e
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 21:40:13 +0530

    Create Greatest.java

---
+++ JavaFundamentals/Arrays/Greatest.java
@@ -0,0 +1,21 @@
+/*
+Write a program to find greatest number in a 3*3 array. The program is supposed to 
+receive 9 integer numbers as command line arguments.
+
+Example1:
+
+     C:\>java Sample 1 2 3
+
+     O/P Expected : Please enter 9 integer numbers
+
+Example2:
+
+     C:\>java Sample 1 23 45 55 121 222 56 77 89
+
+     O/P Expected : 
+The given array is :
+1 23 45 
+55 121 222 
+56 77 89 
+The biggest number in the given array is 222
+*/
