650438eafdce8d0afc36205a207dc39b6bb44687
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 19:36:06 +0530

    Update ReverseNumber.java

--- JavaFundamentals/FlowControlStatements/ReverseNumber.java
+++ JavaFundamentals/FlowControlStatements/ReverseNumber.java
@@ -7,3 +7,28 @@ Eg2)
 I/P:1004
 O/P:4001
 */
+import java.util.Scanner;
+class ReverseNumber
+{
+  public static void main(String args[])
+  {
+    System.out.println("Enter the number:");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int m=0;
+    while(a!=0)
+    {
+      m=10*m+(a%10);
+      a=a/10;
+    }
+    System.out.println("Reverse is:"+m);
+  }
+}
+/*
+OUTPUT:-
+aditya@aditya-Lenovo-G50-80:~/java$ javac ReverseNumber.java
+aditya@aditya-Lenovo-G50-80:~/java$ java ReverseNumber
+Enter the number:
+234
+Reverse is:432
+*/
