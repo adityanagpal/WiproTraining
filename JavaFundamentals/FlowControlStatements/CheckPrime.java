b070eea97901e109c25be7bfc5dbbaed53aa8854
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 19:42:19 +0530

    Update CheckPrime.java

--- JavaFundamentals/FlowControlStatements/CheckPrime.java
+++ JavaFundamentals/FlowControlStatements/CheckPrime.java
@@ -31,3 +31,39 @@ Example5:
 
      O/P Expected : 7 is a prime number
 */
+import java.util.Scanner;
+class Prime
+{
+  public static void main(String args[])
+  {
+   Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the Number:");
+    int n = sc.nextInt();
+       if(n==0||n==1)
+            System.out.println("Neither Prime nor Composite");
+    int d=0;
+    for(int i=2;i<n/2;i++)
+    {
+      if(n%i==0)
+      {
+        d=1;
+        break;
+      }
+    }
+    if(d==0)
+      System.out.println("Prime Number");
+    else
+      System.out.println("Composite Number");
+  }
+}
+/*
+aditya@aditya-Lenovo-G50-80:~/java$ javac Prime.java
+aditya@aditya-Lenovo-G50-80:~/java$ java Prime
+Enter the Number:
+7
+Prime Number
+aditya@aditya-Lenovo-G50-80:~/java$ java Prime
+Enter the Number:
+1
+Neither Prime nor Composite
+*/
