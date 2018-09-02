66431f36aff8a09efc103e3049cc54f2fdda7d86
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 19:58:53 +0530

    Update Divisibles.java

--- JavaFundamentals/FlowControlStatements/Divisibles.java
+++ JavaFundamentals/FlowControlStatements/Divisibles.java
@@ -1,3 +1,25 @@
 /*
 Write a program to print first 5 values which are divisible by 2, 3, and 5.
 */
+class Divisibles
+{
+  public static void main(String args[])
+  {
+    int d=0;
+    int n=30;
+    while(d!=5)
+    {
+      if(n%2==0&&n%3==0&&n%5==0)
+      {
+        System.out.print(n+" ");
+        d++;
+      }
+      n++;
+    }
+  }
+}
+/*
+aditya@aditya-Lenovo-G50-80:~/java$ javac Divisibles.java
+aditya@aditya-Lenovo-G50-80:~/java$ java Divisibles
+30 60 90 120 150
+*/
