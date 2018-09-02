9e9228f84842272a8056f5f9d2120a3940a8ef11
AMAN VERMA <31860233+jnvamanverma@users.noreply.github.com>
2018-09-02 23:35:36 +0530

    Update Ascii.java

--- JavaFundamentals/Arrays/Ascii.java
+++ JavaFundamentals/Arrays/Ascii.java
@@ -1,3 +1,13 @@
-/*
+public class Ascii {
+
+    public static void main(String[] args) {
+        int[] arr = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            arr[i] = Integer.parseInt(args[i]);
+	    System.out.print((char)arr[i]);
+        }
+        
+    }
+}/*
 Initialize an integer array with ascii values and print the corresponding character values in a single row.
 */
