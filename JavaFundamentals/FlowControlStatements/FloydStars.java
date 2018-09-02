98e5a7cca7a13103851b90ed47159067ceea1823
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 17:55:31 +0530

    Update FloydStars.java

--- JavaFundamentals/FlowControlStatements/FloydStars.java
+++ JavaFundamentals/FlowControlStatements/FloydStars.java
@@ -19,3 +19,26 @@ Example1:
                    *  * 
                    *  *  *
 */
+class FloydStars
+{
+ public static void main(String args[])
+ {
+  int n = Integer.parseInt(args[0]);
+  for(int i=0;i<n;i++)
+  {
+   for(int j=0;j<i+1;j++)
+   {
+    System.out.print("* ");
+   }
+   System.out.println();
+  }
+ }
+}
+/*
+aditya@aditya-Lenovo-G50-80:~/java$ javac FloydStars.java
+aditya@aditya-Lenovo-G50-80:~/java$ java FloydStars 4
+* 
+* * 
+* * * 
+* * * * 
+*/
