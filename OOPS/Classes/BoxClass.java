ed5ad3233bcae2d254da7f0e965d7d76ce9a906b
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 21:48:06 +0530

    Create BoxClass.java

---
+++ OOPS/Classes/BoxClass.java
@@ -0,0 +1,5 @@
+/*
+Create a class Box that uses a parameterized method to initialize the dimensions of a box.
+(dimensions are width, height, depth of double type). The class should have a method that
+can return volume. Obtain an object and print the corresponding volume in main() function.
+*/
