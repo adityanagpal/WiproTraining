1ba5a520c4134de99e907439bebe93305326f806
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 17:18:03 +0530

    Create FloydStars.java

---
+++ JavaFundamentals/FlowControlStatements/FloydStars.java
@@ -0,0 +1,21 @@
+/*
+ Write a program to print * in Floyds format (using for and while loop)
+*
+*  *
+*  *   *
+
+Example1:
+
+     C:\>java Sample 
+
+     O/P Expected : Please enter an integer number
+
+Example1:
+
+     C:\>java Sample 3
+
+     O/P Expected :
+                   *
+                   *  * 
+                   *  *  *
+*/
