3d3e737361478f8a1c51a8a5125fd642fed7a818
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 20:05:16 +0530

    Update Even.java

--- JavaFundamentals/FlowControlStatements/Even.java
+++ JavaFundamentals/FlowControlStatements/Even.java
@@ -1,3 +1,36 @@
 /*
  Write a program to print even numbers between 23 and 57, each number should be printed in a separate row.
 */
+class Even
+{
+ public static void main(String args[])
+ {
+  for(int i=23;i<58;i++)
+  {
+   if(i%2==0)
+    System.out.println(i);
+  }
+ }
+}
+/*
+OUTPUT:-
+aditya@aditya-Lenovo-G50-80:~/java$ javac Even.java
+aditya@aditya-Lenovo-G50-80:~/java$ java Even
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+50
+52
+54
+56
+*/
