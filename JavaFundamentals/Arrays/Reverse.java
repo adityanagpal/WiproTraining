79172dfd69a84cbf0f98895b31b4c76ce22d1466
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 21:37:56 +0530

    Create Reverse.java

---
+++ JavaFundamentals/Arrays/Reverse.java
@@ -0,0 +1,22 @@
+/*
+Write a program to reverse the elements of a given 2*2 array. Four integer numbers needs to be passed as Command Line arguments.
+
+Example1:
+
+     C:\>java Sample 1 2 3
+
+     O/P Expected : Please enter 4 integer numbers
+
+Example2:
+
+     C:\>java Sample 1 2 3 4
+
+     O/P Expected : 
+     
+  The given array is :
+  1 2 
+  3 4 
+  The reverse of the array is :
+  4 3 
+  2 1
+*/
