1b499bab40681f874448cbaae1152096debc80ea
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 17:09:03 +0530

    Create Numbers.java

---
+++ JavaFundamentals/FlowControlStatements/Numbers.java
@@ -0,0 +1,17 @@
+/*
+Write a program to print numbers from 1 to 10 in a single row with one tab space.
+*/
+class Numbers
+{
+publc staic void main(String args[])
+{
+for(int i=0;i<10;i++)
+System.out.print((i+1)+" ");
+}
+}
+/*
+OUTPUT:-
+aditya@aditya-Lenovo-G50-80:~/java$ javac Numbers.java
+aditya@aditya-Lenovo-G50-80:~/java$ java Numbers
+1 2 3 4 5 6 7 8 9 10
+*/
