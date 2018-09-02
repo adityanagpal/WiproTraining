2b9c09677b02b81b53b83e186b6140e05b7e8447
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 19:47:09 +0530

    Update DigitSum.java

--- JavaFundamentals/FlowControlStatements/DigitSum.java
+++ JavaFundamentals/FlowControlStatements/DigitSum.java
@@ -1,3 +1,27 @@
 /*
 Write a program to add all the values in a given number and print. Ex: 1234->10
 */
+import java.util.Scanner;
+class DigitSum
+{
+  public static void main(String args[])
+  {
+    System.out.println("Enter the number:");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int m=0;
+    while(a!=0)
+    {
+      m=m+(a%10);
+      a=a/10;
+    }
+    System.out.println("Sum of Digits is:"+m);
+  }
+}
+/*
+aditya@aditya-Lenovo-G50-80:~/java$ javac DigitSum.java
+aditya@aditya-Lenovo-G50-80:~/java$ java DigitSum
+Enter the number:
+246
+Sum of Digits is:12
+*/
