ef6a4383774a506e6b57a009e2b4f430818b54bf
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 17:33:46 +0530

    Create Operation.java

---
+++ JavaFundamentals/FlowControlStatements/Operation.java
@@ -0,0 +1,7 @@
+/*
+Write a program that displays a menu with options 1. Add 2. Sub
+Based on the options chosen, read 2 numbers and perform the relevant operation. After performing 
+the operation, the program should ask the user if he wants to continue. If the user presses y or
+Y, then the program should continue displaying the menu else the program should terminate.
+[ Note: Use Scanner class, you can take help from the trainer regarding the same ]
+*/
