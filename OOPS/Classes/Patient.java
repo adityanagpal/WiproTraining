befa1c74c89f5ccb0ece73eb7ce5d9622f4e8445
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-02 21:51:48 +0530

    Create Patient.java

---
+++ OOPS/Classes/Patient.java
@@ -0,0 +1,12 @@
+/*
+Design a class that can be used by a health care professional to keep track of a patient’s vital statistics.
+Here’s what the class should do: 
+1. Construct a class called Patient
+2. Store a String name for the patient
+3. Store weight and height for patient as doubles
+4. Construct a new patient using these values
+5. Write a method called BMI which returns the patient’s BMI as a double. BMI can be calculated as
+   BMI = ( Weight in Pounds / ( Height in inches x Height in inches ) ) x 703
+6. Next, construct a class called “Patients” and create a main method. Create a Patient object and assign 
+   some height and weight to that object. Display the BMI of that patient.
+*/
