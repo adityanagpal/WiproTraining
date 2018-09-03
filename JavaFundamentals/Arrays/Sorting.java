603b56cdb9516bbc017778e2871f6204ff62f40f
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-03 13:55:33 +0530

    Update Sorting.java

--- JavaFundamentals/Arrays/Sorting.java
+++ JavaFundamentals/Arrays/Sorting.java
@@ -1,3 +1,27 @@
 /*
 Write a program to initialize an array and print them in a sorted fashion
 */
+class Sorting
+{
+  public static void main(String args[])
+  {
+    int l = args.length;
+    int[] a= new int[l];
+    for(int i=0;i<l;i++)
+    a[i]=Integer.parseInt(args[i]);
+    for(int i=0;i<l-1;i++)
+    {
+      for(int j=i+1;j<l;j++)
+      {
+        if(a[i]>a[j])
+        {
+          a[i]=a[i]+a[j];
+          a[j]=a[i]-a[j];
+          a[i]=a[i]-a[j];
+        }
+      }
+    }
+    for(int i=0;i<l;i++)
+      System.out.print(a[i]+" ");
+  }
+}
