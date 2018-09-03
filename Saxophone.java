61ac5f782bbd7d284b18c6c9ff6588cb91ddf6de
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-03 22:31:25 +0530

    Add files via upload

---
+++ Saxophone.java
@@ -0,0 +1,14 @@
+package music.wind;
+
+import music.Playable;
+
+public class Saxophone implements Playable{
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("Saxophone playing....");
+		
+	}
+
+}
