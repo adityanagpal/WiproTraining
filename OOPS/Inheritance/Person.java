ecb539485ae24d817c9d0c02a9f9be88e357fe30
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-03 21:21:12 +0530

    Update Person.java

--- OOPS/Inheritance/Person.java
+++ OOPS/Inheritance/Person.java
@@ -6,3 +6,46 @@ insurance number which is a String.Save this in a file called Employee.java
 Your class should have a reasonable number of constructors and accessor methods.
 Write another class called TestEmployee, containing a main method to fully test your class definition.
 */
+class Person
+{
+  public String name;
+  Person(String n)
+  {
+    name=n;
+  }
+  public void showPerson()
+  {
+    System.out.println("Name = "+name);
+  }
+}
+/*
+in the file Employee.java the first command at top is
+import Person;
+then the remaining code is here
+*/
+class Employee extends Person
+{
+  public double annual_salary;
+  public int start_year;
+  public String insurance_no;
+  Employee(double a,int s,String i)
+  {
+    annual_salary=a; start_year=s; insurance_no=i;
+  }
+  showEmployee()
+  {
+    System.out.println("Annual salary="+annaual_salary);
+    System.out.println("Start year="+start_year);
+    System.out.println("Insurance Number="+insurance_no);
+  }
+}
+class TestEmployee
+{
+  public static void main(String args[])
+  {
+    Persn p=new Person("Subhash");
+    p.showPerson();
+    Employee e=new Employee(35000,2014,"1234");
+    e.showEmployee();
+  }
+}
