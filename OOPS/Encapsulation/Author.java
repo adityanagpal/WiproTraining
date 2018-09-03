482a0b76e22dcfa6e7458294227a11a3600be7ab
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-03 18:17:33 +0530

    Update Author.java

--- OOPS/Encapsulation/Author.java
+++ OOPS/Encapsulation/Author.java
@@ -7,12 +7,72 @@ It contains:
 
 And, a class called Book is designed as follows:
 It contains:
-• Four private instance variables: name (String), author (of the class Author you have just created), price (double), and qtyInStock (int). Assuming that each book is written by one author.
+• Four private instance variables: name (String), author (of the class Author you have just created),
+  price (double), and qtyInStock (int). Assuming that each book is written by one author.
 • One constructor which constructs an instance with the values given.
-• Getters and setters: getName(), getAuthor(), getPrice(), setPrice(), getQtyInStock(), setQtyInStock(). Again there is no setter for name and author.
+• Getters and setters: getName(), getAuthor(), getPrice(), setPrice(), getQtyInStock(), setQtyInStock(). Again
+  there is no setter for name and author.
 Write the class Book (which uses the Author class written earlier). 
 Try:
 1. Printing the book name, price and qtyInStock from a Book instance. (Hint: aBook.getName())
 2. After obtaining the “Author” object, print the Author (name, email & gender) of the book.
 
 */
+import java.util.Scanner;
+class Author
+{
+  private String name,email;
+  private char gender;
+  Author(String n,String e,char g)
+  {
+    name=n; email=e; gender=g;
+  }
+  public void showAuthor()
+  {
+    System.out.println("Name="+name+" Email="+email+" Gender="+gender);
+  }
+}
+class Book
+{
+  Scanner sc=new Scanner(System.in)
+  private String name;
+  private Author author;
+  private double price;
+  private int qtyInStock;
+  public void getName()
+  {
+    name=sc.nextLine();
+  }
+  public void getAuthor()
+  {
+    author=sc.nextLine();
+  }
+  public void getPrice()
+  {
+    price=sc.nextDouble();
+  }
+  public void setPrice(double p)
+  {
+    price=p;
+  }
+  public void getQtyInStock()
+  {
+    qtyInStock=nextInt();
+  }
+  public void setQtyInStock(int q)
+  {
+    qtyInStock=q;
+  }
+  public static void main(String args[])
+  {
+    Book abook=new Book();
+    abook.getname();
+    System.out.println(name);
+    abook.getQtyInStock();
+    System.out.println(qtyInStock);
+    abook.getPrice();
+    System.out.println(price);
+    author = Author("Subhash","subhash123@gmail.com",'m');
+    author.showAuthor();
+  }
+}
