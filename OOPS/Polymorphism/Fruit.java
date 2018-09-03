324561eb50d143fbc5a4546226ee5aee97dc21ce
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-03 22:02:49 +0530

    Update Fruit.java

--- OOPS/Polymorphism/Fruit.java
+++ OOPS/Polymorphism/Fruit.java
@@ -3,3 +3,48 @@ Create  a base class Fruit which has name ,taste and size as its attributes. A m
 describes the name of the fruit and its taste.  Inherit the same in 2 other class Apple and Orange and override the
 eat() method to represent each fruit taste.
 */
+class Fruit
+{
+  public String name,taste;
+  public double size;
+  Fruit(String n,String t,double s)
+  {
+    name=n; taste=t; size=s;
+  }
+  public void eat()
+  {
+    System.out.println("Name="+name+" Taste="+taste);
+  }
+}
+class Apple
+{
+  Apple(String n,String t,double s)
+  {
+    name=n; taste=t; size=s;
+  }
+  public void eat()
+  {
+    System.out.println("Name="+name+" Taste="+taste);
+  }
+}
+class Orange
+{
+  Orange(String n,String t,double s)
+  {
+    name=n; taste=t; size=s;
+  }
+  public void eat()
+  {
+    System.out.println("Name="+name+" Taste="+taste);
+  }
+}
+class Fruits
+{
+  public static void main(String args[])
+  {
+    Apple a=new Apple("apple","sweet",5);
+    a.eat();
+    Oranhe o=new Orange("orange","sour",5);
+    o.eat(); 
+  }
+}
