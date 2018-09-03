4b5fe0f677e8075e55c5273aa540c2a97bea387f
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-03 21:33:01 +0530

    Update PersonStudent.java

--- OOPS/Inheritance/PersonStudent.java
+++ OOPS/Inheritance/PersonStudent.java
@@ -6,3 +6,21 @@
  class by adding a year (current level in college) and major (e.g. “Electrical Engineering”, “Communications”, “Undeclared”).
 
 */
+class Person
+{
+ public String name;
+}
+class Student extends Person
+{
+  public int rollno;
+}
+class Teacher extends Person
+{
+  public int salary;
+  public String subject;
+}
+class CollegeStudent extends Student
+{
+  public int year;
+  public String major;
+}
