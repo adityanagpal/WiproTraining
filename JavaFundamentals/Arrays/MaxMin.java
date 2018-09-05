32162942f18707d7302a3084871296b8b3f22e9a
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-09-05 21:55:27 +0530

    Update MaxMin.java

--- JavaFundamentals/Arrays/MaxMin.java
+++ JavaFundamentals/Arrays/MaxMin.java
@@ -1,3 +1,27 @@
 /*
 Write a program to initialize an integer array and find the maximum and minimum value of an array.
 */
+import java.util.Scanner;
+class MaxMin
+{
+  public static void main(String args[])
+  {
+    int max,min
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of elements to be check");
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    System.out.println("Enter the "+n+" elements in array");
+    for(int i=0;i<n;i++)
+      a[i]=sc.nextInt();
+    max=min=a[0];
+    for(int i=1;i<n;i++)
+    {
+      if(a[i]>max)
+        max=a[i];
+      if(a[i]<min)
+        min=a[i];
+    }
+    System.out.println("Max="+max+" and Min="+min);
+  }
+}
